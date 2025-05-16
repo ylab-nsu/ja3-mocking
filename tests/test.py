ead69f2fe3255a00f4be71e9f4dba12049368929
malaartyom <malarenkoart@bk.ru>
2025-05-16 20:25:31 +0700

    Update tests

--- tests/test.py
+++ tests/test.py
@@ -3,21 +3,21 @@ from src.wireshark_parsing import (get_hash, get_JA3_from_packet,
 
 
 def test1():
-    file_name = "test2.pcap"
+    file_name = "./tests/test2.pcap"
     packets = read_wireshark_file(file_name)
     for pkt in packets:
         assert get_JA3_from_packet(pkt) == get_hash(packets[pkt])
 
 
 def test2():
-    file_name = "test.pcapng"
+    file_name = "./tests/test.pcapng"
     packets = read_wireshark_file(file_name)
     for pkt in packets:
         assert get_JA3_from_packet(pkt) == get_hash(packets[pkt])
 
 
 def test3():
-    file_name = "test3.pcap"
+    file_name = "./tests/test3.pcap"
     packets = read_wireshark_file(file_name)
     for pkt in packets:
         assert get_JA3_from_packet(pkt) == get_hash(packets[pkt])
